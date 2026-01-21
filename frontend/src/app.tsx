02e3001e25f1160db75b6e5761131fc76563dbb9
Leon van der Grient <leon@devtastic.io>
2026-01-21 09:36:38 +0100

    feat: use correct dayjs locale

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,7 +1,15 @@
 import "./index.css";
 import "./i18n";
+// Dayjs locales
+import "dayjs/locale/en";
+import "dayjs/locale/nl";
+import "dayjs/locale/fr";
+import "dayjs/locale/de";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import dayjs from "dayjs";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { Outlet } from "react-router";
 
 import { ConfirmDialogProvider } from "@/components/confirm-dialog-provider";
@@ -10,6 +18,12 @@ import { Toaster } from "@/components/ui/sonner";
 const queryClient = new QueryClient();
 
 export function App() {
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    dayjs.locale(i18n.language);
+  }, [i18n.language]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ConfirmDialogProvider>
