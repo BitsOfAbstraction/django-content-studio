481a965a2d1f301df30bba9f337a5ed50c2dd988
Leon van der Grient <leon@devtastic.io>
2026-02-16 16:00:23 +0100

    feat: add loading bar

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -7,10 +7,12 @@ import "dayjs/locale/nl";
 import "dayjs/locale/fr";
 import "dayjs/locale/de";
 
+import { useIsFetching } from "@tanstack/react-query";
 import dayjs from "dayjs";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { Outlet } from "react-router";
+import LoadingBar, { type LoadingBarRef } from "react-top-loading-bar";
 
 import { ConfirmDialogProvider } from "@/components/confirm-dialog-provider";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
@@ -20,13 +22,24 @@ import { useAdminInfo } from "@/hooks/use-admin-info";
 export function App() {
   const { t, i18n } = useTranslation();
   const { isError, error } = useAdminInfo();
+  const ref = useRef<LoadingBarRef>(null);
+  const isFetching = useIsFetching();
 
   useEffect(() => {
     dayjs.locale(i18n.language);
   }, [i18n.language]);
 
+  useEffect(() => {
+    if (isFetching > 0) {
+      ref.current?.start();
+    } else {
+      ref.current?.complete();
+    }
+  }, [isFetching]);
+
   return (
     <>
+      <LoadingBar ref={ref} color="#1E2938" shadow={false} />
       <ConfirmDialogProvider>
         {isError ? (
           <div className="min-h-screen bg-gray-50 flex items-center justify-center  p-4">
